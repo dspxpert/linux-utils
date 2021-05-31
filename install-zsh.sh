dd0fb2417662499e43d597ab00b9a65462cbbfcb
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-05-31 10:33:20 +0900

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,3 +1,6 @@
+cp MesloLGS* ~/.fonts
+sudo fc-cache -fv
+
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
