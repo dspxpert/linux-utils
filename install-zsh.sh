5351694f543793278817307737396d639915cf34
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-05-31 11:03:50 +0900

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -4,6 +4,7 @@ sudo fc-cache -fv
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
-vi ~/.zshrc
+#vi ~/.zshrc
 #ZSH_THEME="powerlevel10k/powerlevel10k"
+sed -i "/ZSH_THEME=\"/c\ZSH_THEME=\"powerlevel10k/powerlevel10k\"" ~/.zshrc
 #chsh -s $(which zsh)
