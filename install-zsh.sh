3ff1dfc1b37d64aa2351560d516fdb84631db404
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-05-31 10:29:17 +0900

    Create install-zsh.sh

---
+++ install-zsh.sh
@@ -0,0 +1,6 @@
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+vi ~/.zshrc
+#ZSH_THEME="powerlevel10k/powerlevel10k"
+#chsh -s $(which zsh)
