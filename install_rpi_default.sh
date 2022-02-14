9d32814081c4c242a19a55d30cf411eb5f513797
dspxpert <32154318+dspxpert@users.noreply.github.com>
2022-02-14 12:22:38 +0900

    Update install_rpi_default.sh

--- install_rpi_default.sh
+++ install_rpi_default.sh
@@ -1,24 +1,39 @@
 sudo apt update
-sudo apt install -y htop net-tools iperf tmux byobu kdiff3 python3-pip putty neofetch tilix ibus ibus-hangul fonts-nanum exfat-fuse zip unzip hdparm etherwake
 
+sudo usermod -aG docker $USER
+sudo usermod -aG dialout $USER
+
+# Setup fonts
+mkdir -p ~/.fonts
+cp fonts/* ~/.fonts
+sudo fc-cache -fv
+
+# Setup Pictures
+cp pictures/* ~/Pictures
+
+# Install recommanded apps
+sudo apt install -y htop net-tools iperf tmux byobu kdiff3 python3-pip putty neofetch tilix ibus ibus-hangul fonts-nanum exfat-fuse zip unzip hdparm etherwake
 sudo apt install -y ./ulauncher_5.14.3_all.deb
 
-chmod +x ./install-zsh.sh
-./install-zsh.sh
 
+# Setup zsh & powerlevel10k
+chmod +x ./install_zsh.sh
+./install_zsh.sh
+
+# Install Python packages
 pip3 install wpm
 pip3 install -U pyvisa
 pip3 install pyvisa-py
 
+# Setup utils
 chmod +x ipadd
 chmod +x ipdel
 chmod +x wol
-
 cp ./ipadd ~/.local/bin
 cp ./ipdel ~/.local/bin
 cp ./wol   ~/.local/bin
 
-# Swap change
+# Swap Change
 sudo dphys-swapfile swapoff
 sudo vi /etc/dphys-swapfile
 sudo dphys-swapfile setup
