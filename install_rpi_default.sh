30f2a52c78494728d1f24b2dbe872e04c2e718b8
dspxpert <32154318+dspxpert@users.noreply.github.com>
2022-02-14 14:13:09 +0900

    Update install_rpi_default.sh

--- install_rpi_default.sh
+++ install_rpi_default.sh
@@ -11,6 +11,10 @@ sudo fc-cache -fv
 # Setup Pictures
 cp pictures/* ~/Pictures
 
+# Setup scripts to ~/.local/bin
+chmod +x ./scripts/*
+cp ./scripts/* ~/.local/bin
+
 # Install recommanded apps
 sudo apt install -y htop net-tools iperf tmux byobu kdiff3 python3-pip putty neofetch tilix ibus ibus-hangul fonts-nanum exfat-fuse zip unzip hdparm etherwake
 wget https://github.com/Ulauncher/Ulauncher/releases/download/5.14.3/ulauncher_5.14.3_all.deb
@@ -24,15 +28,6 @@ chmod +x ./install_zsh.sh
 pip3 install wpm
 pip3 install -U pyvisa
 pip3 install pyvisa-py
-
-# Setup utils
-cp ./ipadd ~/.local/bin
-cp ./ipdel ~/.local/bin
-cp ./wol   ~/.local/bin
-chmod +x ~/.local/bin/ipadd
-chmod +x ~/.local/bin/ipdel
-chmod +x ~/.local/bin/wol
-
 
 # Swap Change
 sudo dphys-swapfile swapoff
