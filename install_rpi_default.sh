8cd5f385061a5eda236d44b14626fc39443c1266
dspxpert <32154318+dspxpert@users.noreply.github.com>
2022-02-14 12:14:14 +0900

    Update install_rpi_default.sh

--- install_rpi_default.sh
+++ install_rpi_default.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install -y htop net-tools iperf tmux byobu kdiff3 python3-pip putty neofetch tilix ibus ibus-hangul fonts-nanum exfat-fuse zip unzip hdparm
 
-sudo apt install ./ulauncher_5.14.3_all.deb
+sudo apt install -y ./ulauncher_5.14.3_all.deb
 
 chmod +x ./install-zsh.sh
 ./install-zsh.sh
@@ -9,6 +9,14 @@ chmod +x ./install-zsh.sh
 pip3 install wpm
 pip3 install -U pyvisa
 pip3 install pyvisa-py
+
+chmod +x ipadd
+chmod +x ipdel
+chmod +x wol
+
+cp ./ipadd ~/.local/bin
+cp ./ipdel ~/.local/bin
+cp ./wol   ~/.local/bin
 
 # Swap change
 sudo dphys-swapfile swapoff
