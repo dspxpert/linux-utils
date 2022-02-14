cbd22c602c494d0906d7bd9992a2f9db338fcbd4
dspxpert <32154318+dspxpert@users.noreply.github.com>
2022-02-14 15:19:27 +0900

    Update install_odriod.sh

--- install_odriod.sh
+++ install_odriod.sh
@@ -18,7 +18,7 @@ chmod +x ./scripts/*
 cp ./scripts/* ~/.local/bin
 
 # Install recommanded apps
-sudo apt install -y htop net-tools iperf tmux byobu python3-pip neofetch exfat-fuse zip unzip hdparm etherwake
+sudo apt install -y htop net-tools iperf tmux byobu python3-pip python3-tk neofetch exfat-fuse zip unzip hdparm etherwake
 #sudo apt install -y kdiff3 putty tilix ibus ibus-hangul fonts-nanum
 #wget https://github.com/Ulauncher/Ulauncher/releases/download/5.14.3/ulauncher_5.14.3_all.deb
 #sudo apt install -y ./ulauncher_5.14.3_all.deb
