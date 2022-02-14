232749d39985852e251730d7600abb6c44994799
dspxpert <32154318+dspxpert@users.noreply.github.com>
2022-02-14 11:32:06 +0900

    Create install_rpi_default.sh

---
+++ install_rpi_default.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt install -y htop net-tools iperf tmux byobu kdiff3 python3-pip putty neofetch tilix ibus ibus-hangul fonts-nanum exfat-fuse zip unzip hdparm
+
+sudo apt install ./ulauncher_5.14.3_all.deb
+
+chmod +x ./install_zsh.sh
+./install_zsh.sh
+
+pip3 install wpm
+pip3 install -U pyvisa
+pip3 install pyvisa-py
+
+# Swap change
+sudo dphys-swapfile swapoff
+sudo vi /etc/dphys-swapfile
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
